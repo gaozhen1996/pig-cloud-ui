96d6ce531a792c05bf38c7e0118bbb57122ac5f7
gaozhen <gaozhen3h@163.com>
2019-04-16 11:22:35 +0800

    header 添加时间

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -10,6 +10,7 @@
         <div class="collapse-btn" @click="collapseChage">
             <i class="el-icon-menu"></i>
         </div>
+        <div class="time">{{this.time}}</div>
         <div class="header-right">
             <div class="header-user-con">
                 <!-- 全屏显示 -->
@@ -50,6 +51,7 @@
     export default {
         data() {
             return {
+                time:'人生不要错过两样东西：最后一班回家的车和一个深爱你的人',
                 collapse: false,
                 fullscreen: false,
                 name: 'linxin',
@@ -64,6 +66,25 @@
             }
         },
         methods:{
+            dateFilter(date){
+                if(date < 10){return "0"+date;}
+                return date;
+            },          
+            getLangDate(){
+                var dateObj = new Date(); //表示当前系统时间的Date对象
+                var year = dateObj.getFullYear(); //当前系统时间的完整年份值
+                var month = dateObj.getMonth()+1; //当前系统时间的月份值
+                var date = dateObj.getDate(); //当前系统时间的月份中的日
+                var day = dateObj.getDay(); //当前系统时间中的星期值
+                var weeks = ["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
+                var week = weeks[day]; //根据星期值，从数组中获取对应的星期字符串
+                var hour = dateObj.getHours(); //当前系统时间的小时值
+                var minute = dateObj.getMinutes(); //当前系统时间的分钟值
+                var second = dateObj.getSeconds(); //当前系统时间的秒钟值
+                var timeValue = "" +((hour >= 12) ? (hour >= 18) ? "晚上" : "下午" : "上午" ); //当前时间属于上午、晚上还是下午
+                let newDate = this.dateFilter(year)+"年"+this.dateFilter(month)+"月"+this.dateFilter(date)+"日 "+" "+this.dateFilter(hour)+":"+this.dateFilter(minute)+":"+this.dateFilter(second);
+                this.time =  newDate+"　"+week;
+            },            
             // 用户名下拉菜单选择事件
             handleCommand(command) {
                 if(command == 'loginout'){
@@ -109,6 +130,7 @@
             if(document.body.clientWidth < 1200){
                 this.collapseChage();
             }
+            setInterval(this.getLangDate, 1000);
         }
     }
 </script>
@@ -122,12 +144,6 @@
         color: #000000;
         background:#F8F8F8
     }
-    .collapse-btn{
-        float: left;
-        padding: 0 21px;
-        cursor: pointer;
-        line-height: 70px;
-    }
 
     .header .logo1 {
         float: left;
@@ -140,8 +156,6 @@
     .header .logo1 font{
         color: white;
     }
-
-
     .header .logo2 {
         float: left;
         width:250px;
@@ -149,11 +163,24 @@
         background:#000000;
         text-align: center;
     }
-
-
     .header .logo2 font{
         color: white;
     }
+
+
+    .collapse-btn{
+        float: left;
+        padding: 0 21px;
+        cursor: pointer;
+        line-height: 70px;
+    }
+
+    .time{
+        float: left;
+        padding: 0 21px;
+        line-height: 70px;
+    }
+
 
     .header-right{
         float: right;
