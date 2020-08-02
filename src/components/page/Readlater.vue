91f59c0c52e2a914b9522024105c687e05ab3a01
gaozhen <gaozhen3h@163.com>
2020-08-02 15:19:49 +0800

    稍后阅读管理完成

--- src/components/page/Readlater.vue
+++ src/components/page/Readlater.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="table">
         <div class="container">
+            <div class="handle-box">
+                <el-button type="primary" icon="search" @click="handleAdd">新建稍后阅读</el-button>
+            </div>            
             <!-- 表格版本 -->
             <el-table :data="data" border class="table">
                 <el-table-column prop="createTime" label="创建时间" width="200">
                 </el-table-column>
-                <el-table-column prop="content" label="收藏内容">
+                <el-table-column prop="title" label="收藏内容">
                 </el-table-column>
                 <el-table-column label="操作" width="180" align="center">
                     <template slot-scope="scope">
@@ -19,6 +22,22 @@
                 </el-pagination>
             </div>
         </div>
+
+        <!-- 新建弹出框 -->
+        <el-dialog title="新建"  :visible.sync="addVisible">
+            <el-form ref="obj" :model="obj" label-width="80px">
+                <el-form-item label="内容">
+                    <el-input v-model="obj.title"></el-input>
+                </el-form-item>
+                <el-form-item label="链接">
+                    <el-input v-model="obj.url"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" @click="addVisible=false">取消</el-button>
+                <el-button type="primary" @click="saveAdd">保存</el-button>
+            </span>
+        </el-dialog>
 
         <!-- 删除提示框 -->
         <el-dialog title="提示" :visible.sync="delVisible" width="300px" center>
@@ -39,7 +58,7 @@
         name: 'basetable',
         data() {
             return {
-                slectVisible: false,
+                addVisible: false,
                 delVisible: false,
                 tableData: [],
                 cur_page: 1,
@@ -47,13 +66,15 @@
                 obj:{
                     id:'',
                     createTime:'',
-                    content:'',
+                    title:'',
                     url:''
                 },
-                idx: -1
+                idx: -1,
+                uid:0,
             }
         },
         created() {
+            this.uid=JSON.parse(localStorage.getItem('user')).id;
             this.getData();
         },
         computed: {
@@ -71,13 +92,16 @@
             },
             getData() {
                 let startRow=10*(this.cur_page-1);
-                axios.get(Global.baseurl+"/video-api/web/rest/videoInfo/getVideoInfoByPage?startRow="
+                axios.get(Global.baseurl+"/plan-api/web/rest/readlater/listReadlaterByUidAndPaging?uid="+this.uid+"&startRow="
                             +startRow+"&rowSize=10")
                 .then((response) => {
-                    var code = response.data.code;
-                    if(code==0){
-                       this.tableData=response.data.data;
-                       this.total=response.data.count-1;
+                    let code = response.data.code;
+                    let responseData = response.data.data;
+                    if(code==2){
+                        if(responseData.row>0){
+                            this.tableData=responseData.data;
+                            this.total=responseData.row-1;
+                        }
                     }else{
                         this.$message.error('亲，错了哦，出了一点小异常');
                     }
@@ -85,15 +109,53 @@
                     console.log(error);
                 });
             },
+            handleAdd() {
+                this.obj={
+                    id:'',
+                    createTime:'',
+                    title:'',
+                    url:''
+                },
+                this.addVisible=true;
+            },
+            //确认新建
+            saveAdd(){
+                let request = {"title":this.obj.title,
+                               "uid":this.uid,
+                               "url":this.obj.url};
+                axios.post(Global.baseurl+"/plan-api/web/rest/readlater/addReadlater",request)
+                .then(res=>{
+                    if(res.data.code==2){
+                        this.addVisible=false;
+                        this.tableData.unshift(res.data.data);
+                        this.$message.success('提交成功！');
+                    }else{
+                        console.log("新增失败")
+                        this.$message.error('亲，错了哦，出了一点小异常');
+                    }         
+                })
+            },                        
             handleDelete(index, row) {
                 this.idx = index;
+                this.obj=this.tableData[this.idx]
                 this.delVisible = true;
             },
             // 确定删除
             deleteRow(){
-                this.tableData.splice(this.idx, 1);
-                this.$message.success('删除成功');
-                this.delVisible = false;
+                let request = {"id":this.obj.id};
+                axios.post(Global.baseurl+"/plan-api/web/rest/readlater/deleteNoteById",request)
+                .then(res=>{
+                    if(res.data.code==2){
+                        this.delVisible=false;
+                        this.$message.success('删除成功！');
+                        this.tableData.splice(this.idx, 1);
+                        this.$message.success('删除成功');
+                        this.delVisible = false;
+                    }else{
+                        console.log("删除失败")
+                        this.$message.error('亲，错了哦，出了一点小异常');
+                    }         
+                })
             }
         }
     }
