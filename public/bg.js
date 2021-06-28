4169863986fe362dffd4a370bf1a9c148159b510
gaozhen <gaozhen3h@163.com>
2021-06-28 23:18:42 +0800

    修改获取html方式

--- public/bg.js
+++ public/bg.js
@@ -1,72 +1,58 @@
-structConfig = {
+structConfig = [{
+	"host":"gitee.com",
 	"type":"id",
-	"name":"git-readme",
-	"child":{
-		"type":"class",
-		"name":"ui flat nopadding segment file_holder",
-		"child":{
-			"type":"class",
-			"name":"file_content markdown-body",
-			"child":{}
-		}		
-	}
+	"name":"file_content markdown-body"
 }
+]
 start()
 
 /**
- * 如果type为则root可以不传
- */
-function getElement(root,type,name){
-	if(type=="id"){
-		return document.getElementById(name)
-	}else if(type=="class"){
-		while(root.hasChildNodes()){
-			if(root.firstChild.className==name){
-				return root.firstChild
-			}
-			root.removeChild(root.firstChild);
-		}
-	}
+* 
+*/
+function getElement(type,name){
+if(type=="id"){
+	return document.getElementById(name)
+}else if(type=="class"){
+	return document.getElementsByClassName('name')[0]
+}
 }
 
 
 function start(){
-	host = window.location.host;
-	if (host.indexOf("gitee.com")!= -1){
-		var parseConfig = structConfig
-		var rootDiv=getElement(rootDiv,parseConfig.type,parseConfig.name)
-		var htmlStruct = rootDiv
-		
-		while(parseConfig.child!=undefined){
-			htmlStruct = getElement(htmlStruct,parseConfig.type,parseConfig.name)
-			parseConfig = parseConfig.child;
-		}
-		console.log(htmlStruct)					
-
-		var minder =  new kityminder.Minder({
-			renderTo: '#git-readme'
-		});
-		console.log(minder)
-		minder.importJson({
-		"root": {
-		  "data": {"text": "test111"},
-		  "children": [
-			{"data": {"text": "网页"}},
-			{"data": {"text": "贴吧"}},
-			{"data": {"text": "知道"}},
-			{"data": {"text": "音乐"}},
-			{"data": {"text": "图片"}},
-			{"data": {"text": "视频"}},
-			{"data": {"text": "地图"}},
-			{"data": {"text": "百科", "expandState": "collapse"}}
-		  ]
-		},
-		"template": "default"
-		});	
+host = window.location.host;
+var htmlStruct;
+for(var i=0;i<structConfig.length;i++){
+	if(host==structConfig[i].host){
+		htmlStruct = getElement(structConfig[i].type,structConfig[i].name)
+		break
 	}
+
+}
+console.log(htmlStruct)					
+
+var minder =  new kityminder.Minder({
+	renderTo: '#git-readme'
+});
+console.log(minder)
+minder.importJson({
+"root": {
+	"data": {"text": "test111"},
+	"children": [
+	{"data": {"text": "网页"}},
+	{"data": {"text": "贴吧"}},
+	{"data": {"text": "知道"}},
+	{"data": {"text": "音乐"}},
+	{"data": {"text": "图片"}},
+	{"data": {"text": "视频"}},
+	{"data": {"text": "地图"}},
+	{"data": {"text": "百科", "expandState": "collapse"}}
+	]
+},
+"template": "default"
+});	
 }
 
 
 function changeToNaotu(){
-	
+
 }
--- src/components/page/VueEditor.vue
+++ src/components/page/VueEditor.vue
@@ -8,8 +8,8 @@
 </template>
 
 <script>
-  import kity from 'kity'
-  import kityminder from 'kityminder-core'
+  // import kity from 'kity'
+  // import kityminder from 'kityminder-core'
 
   export default {
     name: "page",
@@ -18,25 +18,25 @@
     },
     methods: {},
     mounted() {
-      this.km = new window.kityminder.Minder({
-        renderTo: '#minder-container'
-      });
-      this.km.importJson({
-        "root": {
-          "data": {"text": "test111"},
-          "children": [
-            {"data": {"text": "网页"}},
-            {"data": {"text": "贴吧"}},
-            {"data": {"text": "知道"}},
-            {"data": {"text": "音乐"}},
-            {"data": {"text": "图片"}},
-            {"data": {"text": "视频"}},
-            {"data": {"text": "地图"}},
-            {"data": {"text": "百科", "expandState": "collapse"}}
-          ]
-        },
-        "template": "default"
-      });
+      // this.km = new window.kityminder.Minder({
+      //   renderTo: '#minder-container'
+      // });
+      // this.km.importJson({
+      //   "root": {
+      //     "data": {"text": "test111"},
+      //     "children": [
+      //       {"data": {"text": "网页"}},
+      //       {"data": {"text": "贴吧"}},
+      //       {"data": {"text": "知道"}},
+      //       {"data": {"text": "音乐"}},
+      //       {"data": {"text": "图片"}},
+      //       {"data": {"text": "视频"}},
+      //       {"data": {"text": "地图"}},
+      //       {"data": {"text": "百科", "expandState": "collapse"}}
+      //     ]
+      //   },
+      //   "template": "default"
+      // });
     }
   }
 </script>
